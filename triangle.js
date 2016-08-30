7a94a048960fd2160acf9889f5200e3190764513
Mahesh Gaya <mmaheshgaya@gmail.com>
2016-08-30 13:32:12 -0500

    Initial Commit
    Added triangle.html, and triangle.js

---
+++ triangle.js
@@ -0,0 +1,53 @@
+"use strict";
+
+var gl;
+var points;
+
+window.onload = function init()
+{
+    var canvas = document.getElementById( "gl-canvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    //
+    //  Initialize our data for the triangle
+    //
+
+    // initialize the corners with three points.
+    var vertices = [
+        vec2( -1, -1 ),
+        vec2(  0,  1 ),
+        vec2(  1, -1 )
+    ];
+ 
+    // And, add our vertices point into our array of points
+    points = [vertices[0], vertices[1], vertices[2]];
+
+    //  Configure WebGL
+    //
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+    //  Load shaders and initialize attribute buffers
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    // Load the data into the GPU
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+
+    // Associate out shader variables with our data buffer
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    render();
+};
+
+
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.drawArrays( gl.TRIANGLES, 0, points.length );
+}
